d71df078c6d63f71b9bea5f35527ff2dd80c0e39
thomaskisner <tjkisner@gmail.com>
2019-01-11 13:22:40 -0500

    Completed MVP of project. Can open particular directories, or opens default directory if none specified. All contents are printed to console.

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
 #include <dirent.h>
+#include <string.h>
 
 /**
  * Main
@@ -7,12 +10,40 @@
 int main(int argc, char **argv)
 {
   // Parse command line
+    int i; 
+    printf("There are %d command line argument(s):\n", argc);
+    for (i = 0; i < argc; i++) {
+        printf("   %s\n", argv[i]);
+    }
 
   // Open directory
+  DIR * dir;
+  struct dirent *sd;
+  
+  if(argv[1] != NULL){
+    
+    char *path = argv[1];
+    printf("arg 2%s \n", argv[1]);
+     dir = opendir(path) ;
+  } else {
+    dir = opendir(".");
+  } 
+  if(dir == NULL){
+    printf("error, can not open that directory \n");
+    exit(1);
+  }
 
-  // Repeatly read and print entries
-
-  // Close directory
-
-  return 0;
+while( (sd=readdir(dir)) != NULL)
+{
+  printf(">> %s \n", sd->d_name);
 }
+closedir(dir);
+      return 0;
+    }
+
+
+
+
+ 
+
+  
